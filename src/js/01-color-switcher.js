9130d48038fd66c49c89138922a988b0a2ab3b0d
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-08-18 18:25:40 +0300

    first task done

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const elements = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+  body: document.querySelector('body'),
+};
+let colorSet = null;
 
+elements.startBtn.addEventListener('click', handlerStart);
+elements.stopBtn.addEventListener('click', handlerStop);
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function handlerStart(evt) {
+  evt.target.setAttribute('disabled', 'disabled');
+  elements.stopBtn.removeAttribute('disabled');
+  elements.body.style.backgroundColor = getRandomHexColor();
+  colorSet = setInterval(() => {
+    elements.body.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function handlerStop(evt) {
+  elements.startBtn.removeAttribute('disabled');
+  evt.target.setAttribute('disabled', 'disabled');
+  clearInterval(colorSet);
+}
