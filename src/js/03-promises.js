189f0d9863cd6ffdd32f40f5ccdcdd83ecbf544f
Anton Rybalko <126842693+AntonRybalko777@users.noreply.github.com>
2023-08-21 00:29:12 +0300

    added return to the 3rd task

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -33,9 +33,13 @@ function createPromise(position, delay) {
 
   promise
     .then(({ position, delay }) => {
-      Notiflix.Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      return Notiflix.Notify.success(
+        `✅ Fulfilled promise ${position} in ${delay}ms`
+      );
     })
     .catch(({ position, delay }) => {
-      Notiflix.Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+      return Notiflix.Notify.failure(
+        `❌ Rejected promise ${position} in ${delay}ms`
+      );
     });
 }
